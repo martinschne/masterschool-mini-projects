38b03a4440dabd84fe646180cf1c1cc376a52957
Martin Schneider <martin.schne@protonmail.com>
2024-12-16 19:07:19 +0100

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -41,12 +41,16 @@ def main():
 
     for animal in animals_data:
         # serializing to html format
-        output += "<li class='cards__item'>"
-        output += f"Name: {animal["name"]}<br>\n" if "name" in animal else ""
-        output += f"Diet: {animal["characteristics"]["diet"]}<br>\n" if "diet" in animal["characteristics"] else ""
-        output += f"Location: {animal["locations"][0]}<br>\n" if "locations" in animal else ''
-        output += f"Type: {animal["characteristics"]["type"]}<br>" if "type" in animal["characteristics"] else ""
-        output += "</li>"
+        output += "\t\t\t<li class=\"cards__item\">\n"
+        output += f"\t\t\t\t<div class=\"card__title\">{animal["name"]}</div>\n" if "name" in animal else ""
+        output += "\t\t\t\t<p class=\"card__text\">\n"
+        output += f"\t\t\t\t\t<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br>\n" if "diet" in animal["characteristics"] else ""
+        output += f"\t\t\t\t\t<strong>Location:</strong> {animal["locations"][0]}<br>\n" if "locations" in animal else ''
+        output += f"\t\t\t\t\t<strong>Type:</strong> {animal["characteristics"]["type"]}<br>\n" if "type" in animal["characteristics"] else ""
+        output += "\t\t\t\t</p>\n"
+        output += "\t\t\t</li>\n"
+
+    output = output[3:-1] # remove the first tabs and last newline after serializing all items
 
     animals_template_site = load_file("animals_template.html")
     animals_template_site = animals_template_site.replace("__REPLACE_ANIMALS_INFO__", output)
