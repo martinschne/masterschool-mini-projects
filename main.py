4cf1cda31967ea2b3cdebac8e8b99442def4b4bf
Martin Schneider <martin.schne@protonmail.com>
2025-01-20 13:21:54 +0100

    Project code base

---
+++ main.py
@@ -0,0 +1,120 @@
+import sys
+
+from products import Product
+from store import Store
+
+
+def print_store_products(store: Store):
+    """
+    Prints numbered list of all products in the store with their details.
+    :param store: Store object exposing its products for displaying.
+    """
+    print("------")
+    for index, product in enumerate(store.get_all_products()):
+        print(f"{index + 1}. {product.show()}")
+    print("------")
+
+
+def print_store_items_amount(store: Store):
+    """
+    Prints total quantity of all items assigned on store instance.
+    :param store: Store object exposing its products for item counting.
+    """
+    print(f"Total of {store.get_total_quantity()} items in store")
+
+
+def make_order(store: Store):
+    """
+    Navigates user through the order creation process:
+    Display the products available in store.
+    Repeatedly instructs the user to add new item consisting of
+    Product instance and ordered quantity to the shopping list.
+    Upon receiving empty product index or quantity input from user
+    finalizes the order creation with displaying total payment for
+    the order.
+    :param store: Store object exposing its methods for managing ordered products it contains.
+    """
+    shopping_list = []
+
+    print_store_products(store)
+    print("When you want to finish order, enter empty text.")
+
+    while True:
+        selected_product_index = input("Which product # do you want? ")
+        quantity = input("What amount do you want? ")
+
+        if selected_product_index == "" or quantity == "":
+            break
+
+        try:
+            product_index = int(selected_product_index) - 1
+            product = store.get_all_products()[product_index]
+            quantity = int(quantity)
+
+            item = (product, quantity)
+            shopping_list.append(item)
+            print("Product added to list!\n")
+        except (ValueError, IndexError):
+            print("Error adding product!\n")
+
+    if len(shopping_list) > 0:
+        try:
+            total_order = store.order(shopping_list)
+            if total_order != 0:
+                print("********")
+                print(f"Order made! Total payment: ${total_order}")
+        except ValueError:
+            print("Error while making order! Quantity larger than what exists")
+
+
+def start(store: Store):
+    """
+    Provide terminal user interface to the user.
+    Handles menu printing, item selection handles execution of corresponding
+    helper function referenced in 'menu_options' list.
+    :param store: Store object for exposing store methods to helper functions
+    """
+    menu_options = [
+        lambda: print_store_products(store),
+        lambda: print_store_items_amount(store),
+        lambda: make_order(store),
+        sys.exit
+    ]
+
+    while True:
+        print("   Store Menu")
+        print("   ----------")
+        print("1. List all products in store")
+        print("2. Show total amount in store")
+        print("3. Make an order")
+        print("4. Quit")
+
+        try:
+            user_choice = int(input("Please choose a number: "))
+            menu_index = user_choice - 1
+
+            if 0 <= menu_index < len(menu_options):
+                menu_options[user_choice - 1]()
+        except ValueError:
+            print("Error with your choice! Try again!")
+
+        print()
+
+
+def main():
+    """
+    Main method to start the program.
+    """
+    # setup initial stock of inventory
+    product_list = [
+        Product("MacBook Air M2", price=1450, quantity=100),
+        Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+        Product("Google Pixel 7", price=500, quantity=250)
+    ]
+
+    best_buy = Store(product_list)
+    start(best_buy)
+
+
+if __name__ == "__main__":
+    main()
---
+++ products.py
@@ -0,0 +1,81 @@
+class Product:
+    """
+    Represents a product with a name, price, and quantity in stock.
+    """
+
+    def __init__(self, name: str, price: float, quantity: int):
+        """
+        Initializes a Product instance with the given name, price, and quantity.
+        :param name: (str) The name of the product. Must not be empty.
+        :param price: (float) The price of the product. Must be non-negative.
+        :param quantity: (int) The quantity of the product in stock. Must be non-negative.
+        :raises ValueError: If name is empty, price is negative, or quantity is negative.
+        """
+        if name == "":
+            raise ValueError("Invalid name: name cannot be empty")
+        if price < 0:
+            raise ValueError(f"Invalid price: {price}, price can't be negative")
+        if quantity < 0:
+            raise ValueError(f"Invalid quantity: {quantity}, quantity can't be negative")
+
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = quantity > 0
+
+    def get_quantity(self) -> int:
+        """
+        Returns the current quantity of the product in stock.
+        :returns: (int) The quantity of the product.
+        """
+        return self.quantity
+
+    def set_quantity(self, quantity: int):
+        """
+        Set the quantity of the product in stock and updates its active status.
+        :param quantity: (int) The new quantity of the product.
+        :raises ValueError: If the quantity is negative.
+        """
+        if quantity > 0:
+            self.activate()
+        else:
+            self.deactivate()
+
+        self.quantity = quantity
+
+    def is_active(self) -> bool:
+        """
+        Checks if the product is active.
+        :returns: (bool) True if the product is active, False otherwise.
+        """
+        return self.active
+
+    def activate(self):
+        """Activates the product, setting its status to active."""
+        self.active = True
+
+    def deactivate(self):
+        """Deactivates the product, setting its status to inactive."""
+        self.active = False
+
+    def show(self) -> str:
+        """
+        Returns a string representation of the product.
+        :returns A string containing the product's name, price, and quantity.
+        """
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+
+    def buy(self, quantity: int) -> float:
+        """
+        Buys a specified quantity of the product.
+        :param quantity: (int): The quantity to purchase.
+        :returns: (float) The total cost of the purchased quantity.
+        :raises: ValueError: If the requested quantity exceeds the available stock.
+        """
+        product_quantity = self.get_quantity()
+        if quantity > product_quantity:
+            raise ValueError("Requested quantity exceeds the stock")
+
+        self.set_quantity(product_quantity - quantity)
+
+        return quantity * self.price
