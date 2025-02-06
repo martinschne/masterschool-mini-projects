b9c490ed58448a294ce64cb86bd4e88e3a329fe5
Martin Schneider <martin.schne@protonmail.com>
2025-02-06 20:23:12 +0100

    implement update_post endpoint logic and error handling.

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -56,5 +56,20 @@ def delete_post(id):
     return jsonify({"message": f"Post with id {id} has been deleted successfully."}), 200
 
 
+@app.route('/api/posts/<int:id>', methods=['PUT'])
+def update_post(id):
+    global POSTS
+
+    updated_post = next((post for post in POSTS if post["id"] == id), None)
+
+    if updated_post is None:
+        return jsonify({"error": "Post Not Found"}), 404
+
+    updates_sent = request.get_json()
+    updated_post.update(updates_sent)
+
+    return jsonify(updated_post), 200
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
