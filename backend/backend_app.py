e6a60c4fe25b5db2d82b9b0f09713f16e7097f1e
Martin Schneider <martin.schne@protonmail.com>
2025-02-06 16:43:47 +0100

    add requirements.txt, implement add_post route with validation.

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,6 +13,29 @@ POSTS = [
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+def _validate_post(post: dict) -> (bool, str):
+    missing_keys = []
+    if "title" not in post or post["title"] == "":
+        missing_keys.append("title")
+    if "content" not in post or post["content"] == "":
+        missing_keys.append("content")
+
+    return len(missing_keys) == 0, ", ".join(missing_keys)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    new_post = request.get_json()
+
+    is_post_valid, missing_keys = _validate_post(new_post)
+    if not is_post_valid:
+        return jsonify({"error": f"Missing keys: {missing_keys}"}), 400
+
+    POSTS.append(new_post)
+
+    return jsonify(new_post), 201
 
 
 if __name__ == '__main__':
